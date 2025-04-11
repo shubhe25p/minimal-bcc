0d7ddce804a43a71281d0e247ed950eb0c7322a2
Brandon g Cook <bgcook@lbl.gov>
2025-04-11 16:25:13 -0700

    add basic test

--- test/simple-write/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This for someone at NERSC with root to run
-# gcc write.c -o write
-
-./write /global/homes/c/cookbg/ebpf-tmp/test.$(date +%s).dat 4096
-./write /mscratch/sd/c/cookbg/ebpf-tmp/test.$(date +%s).dat $((128 * 1024 * 1024))
-./write /global/cfs/cdirs/nstaff/cookbg/ebpf-tmp/test.$(date +%s).dat $((8 * 1024 * 1024))
